8f9868680789d08cd5a07bdc50f8f9818631bf74
Enrique Fernandez <efernandez@clearpathrobotics.com>
2015-04-01 08:48:05 -0400

    Add nm-applet for awesome

--- install.sh
+++ install.sh
@@ -9,3 +9,14 @@ cp .bash_aliases ~
 
 cp .ssh/config ~/.ssh/config
 
+# nm-applet for awesome
+# https://awesome.naquadah.org/wiki/Nm-applet
+sudo adduser efernandez netdev
+# Create <policy user="efernandez"> ... </policy> blocks
+# taking the root ones for:
+#/etc/dbus-1/system.d/org.freedesktop.NetworkManager.conf
+#/etc/dbus-1/system.d/NetworkManager.conf
+#/etc/dbus-1/system.d/NetworkManagerInfo.conf
+#/etc/dbus-1/system.d/nm-applet.conf
+#/etc/dbus-1/system.d/nm-avahi-autoipd.conf
+#/etc/dbus-1/system.d/nm-dhcp-client.conf
