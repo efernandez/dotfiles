febb7f060e83ecc119a5e52ede7ea91a34b819d8
Enrique Fernandez <efernandez@clearpathrobotics.com>
2015-04-01 18:05:59 -0400

    Add battery indicator source/link

--- battery-indicator/install.sh
+++ battery-indicator/install.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# http://www.everythingisvoid.com/uncategorized/simple-battery-status-indicator-awesome-window-manager#sthash.rLKCN08B.dpbs
 sudo apt-get install -y lua5.1 luarocks libgirepository1.0-dev acpi
 
 sudo luarocks install battery_status
